82c99abe44b254df73fe40db57044edf7cc6b9a2
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-11 09:53:09 -0600

    Updates date handling for more flexibility

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,4 +1,8 @@
+require 'date'
+
 class MostDangerousDayController < ApplicationController
   def index
+    @start_date = DateTime.strptime(params[:start_date], "%Y-%m-%d").strftime("%B %-d, %Y")
+    @end_date = DateTime.strptime(params[:end_date], "%Y-%m-%d").strftime("%B %-d, %Y")
   end
 end
