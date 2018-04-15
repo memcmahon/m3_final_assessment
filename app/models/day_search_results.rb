c9de69669b19b6b26f16d374b83893692aea7370
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-15 14:15:20 -0600

    privatize search result methods

--- app/models/day_search_results.rb
+++ app/models/day_search_results.rb
@@ -6,34 +6,34 @@ class DaySearchResults
     @dangeroids = hazardous_asteroids(data[:near_earth_objects], @most_dangerous_date)
   end
 
-  def self.search(start_date, end_date)
-    new(NasaSearchService.get_danger_day(start_date, end_date))
-  end
-
-  def asteroids_by_day(data)
-    data.transform_values do |value|
-      value.select do |asteroid|
-        asteroid[:is_potentially_hazardous_asteroid]
-      end.count
-    end
-  end
-
-  def danger_day(data)
-    @most_dangerous_date = asteroids_by_day(data).sort_by do |key, value|
-      value
-    end.last[0].to_s
-
-    DateTime.strptime(@most_dangerous_date, "%Y-%m-%d").strftime("%B %-d, %Y")
-  end
-
-  def hazardous_asteroids(data, date)
-    data[:"#{date}"].map do |asteroid|
-      if asteroid[:is_potentially_hazardous_asteroid]
-        Asteroid.find(asteroid)
-      end
-    end.compact
-  end
-
   private
     attr_reader :most_dangerous_date
+
+    def self.search(start_date, end_date)
+      new(NasaSearchService.get_danger_day(start_date, end_date))
+    end
+
+    def asteroids_by_day(data)
+      data.transform_values do |value|
+        value.select do |asteroid|
+          asteroid[:is_potentially_hazardous_asteroid]
+        end.count
+      end
+    end
+
+    def danger_day(data)
+      @most_dangerous_date = asteroids_by_day(data).sort_by do |key, value|
+        value
+      end.last[0].to_s
+
+      DateTime.strptime(@most_dangerous_date, "%Y-%m-%d").strftime("%B %-d, %Y")
+    end
+
+    def hazardous_asteroids(data, date)
+      data[:"#{date}"].map do |asteroid|
+        if asteroid[:is_potentially_hazardous_asteroid]
+          Asteroid.find(asteroid)
+        end
+      end.compact
+    end
 end
