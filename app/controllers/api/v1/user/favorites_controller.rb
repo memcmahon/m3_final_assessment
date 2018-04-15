adc4a06aac29fa6e0bc99f5ad3750a60413219d1
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-15 12:56:56 -0600

    refactors favorites controller

--- app/controllers/api/v1/user/favorites_controller.rb
+++ app/controllers/api/v1/user/favorites_controller.rb
@@ -2,16 +2,15 @@ class Api::V1::User::FavoritesController < ApplicationController
   before_action :find_user
   def index
     favorites = @user.favorites.map do |favorite|
-      AsteroidSearchService.find(favorite)
+      favorite_asteroids(favorite)
     end
-
     render json: favorites
   end
 
   def create
     favorite = @user.favorites.new(neo_reference_id: params[:neo_reference_id])
     if favorite.save
-      favorite = AsteroidSearchService.find(favorite)
+      favorite = favorite_asteroids(favorite)
       render json: favorite
     end
   end
@@ -19,5 +18,9 @@ class Api::V1::User::FavoritesController < ApplicationController
   private
     def find_user
       @user = ApiKey.find_by(value: params["api_key"]).user
+    end
+
+    def favorite_asteroids(favorite)
+      AsteroidSearchService.find(favorite)
     end
 end
