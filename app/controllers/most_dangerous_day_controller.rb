c8266fd649db0910967fc52a14818006fc8605da
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-15 10:51:52 -0600

    Functional with one api call

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -2,7 +2,9 @@ class MostDangerousDayController < ApplicationController
   def index
     @start_date = DateTime.strptime(params[:start_date], "%Y-%m-%d").strftime("%B %-d, %Y")
     @end_date = DateTime.strptime(params[:end_date], "%Y-%m-%d").strftime("%B %-d, %Y")
-    @most_dangerous_day = DangerDaySearchService.most_dangerous_day(params[:start_date], params[:end_date])
-    @dangeroids = @most_dangerous_day.dangeroids
+    @search_result = DangerDaySearchService.most_dangerous_day(params[:start_date], params[:end_date])
+    # binding.pry
+    # @most_dangerous_day = DangerDaySearchService.most_dangerous_day(params[:start_date], params[:end_date])
+    # @dangeroids = @most_dangerous_day.dangeroids
   end
 end
--- app/models/day_search_results.rb
+++ app/models/day_search_results.rb
@@ -2,8 +2,8 @@ class DaySearchResults
   attr_reader :dangeroids, :most_dangerous_day
 
   def initialize(data)
-    @most_dangerous_day = most_dangerous_day(data[:near_earth_objects])
-    @dangeroids = dangeroids(data[:near_earth_objects], @most_dangerous_date)
+    @most_dangerous_day = danger_day(data[:near_earth_objects])
+    @dangeroids = hazardous_asteroids(data[:near_earth_objects], @most_dangerous_date)
   end
 
   def asteroids_by_day(data)
@@ -14,8 +14,7 @@ class DaySearchResults
     end
   end
 
-  def most_dangerous_day(data)
-    binding.pry
+  def danger_day(data)
     @most_dangerous_date = asteroids_by_day(data).sort_by do |key, value|
       value
     end.last[0].to_s
@@ -23,8 +22,12 @@ class DaySearchResults
     DateTime.strptime(@most_dangerous_date, "%Y-%m-%d").strftime("%B %-d, %Y")
   end
 
-  def dangeroids(data, date)
-    binding.pry
+  def hazardous_asteroids(data, date)
+    data[:"#{date}"].map do |asteroid|
+      if asteroid[:is_potentially_hazardous_asteroid]
+        Asteroid.new(asteroid)
+      end
+    end.compact
   end
 
   private
