f10492ca3e731b6f1425173b841b0225e3af060d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-15 11:06:06 -0600

    Refactors danger day search service

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -2,9 +2,6 @@ class MostDangerousDayController < ApplicationController
   def index
     @start_date = DateTime.strptime(params[:start_date], "%Y-%m-%d").strftime("%B %-d, %Y")
     @end_date = DateTime.strptime(params[:end_date], "%Y-%m-%d").strftime("%B %-d, %Y")
-    @search_result = DangerDaySearchService.most_dangerous_day(params[:start_date], params[:end_date])
-    # binding.pry
-    # @most_dangerous_day = DangerDaySearchService.most_dangerous_day(params[:start_date], params[:end_date])
-    # @dangeroids = @most_dangerous_day.dangeroids
+    @search_result = DangerDaySearchService.date_range_search(params[:start_date], params[:end_date])
   end
 end
--- app/services/danger_day_search_service.rb
+++ app/services/danger_day_search_service.rb
@@ -1,34 +1,19 @@
 class DangerDaySearchService
-  def self.most_dangerous_day(start_date, end_date)
-    conn = Faraday.new("https://api.nasa.gov/neo/rest/v1/feed")
-
+  def self.date_range_search(start_date, end_date)
     response = conn.get do |req|
       req.params["start_date"] = start_date
       req.params["end_date"] = end_date
       req.params["api_key"] = ENV["NASA_API_KEY"]
     end
 
-    raw_data = JSON.parse(response.body, symbolize_names: true)
-
-    DaySearchResults.new(raw_data)
+    DaySearchResults.new(parse(response))
   end
 
-  # def self.dangeroids(dangerday)
-  #   conn = Faraday.new("https://api.nasa.gov/neo/rest/v1/feed")
-  #
-  #   response = conn.get do |req|
-  #     req.params["start_date"] = dangerday
-  #     req.params["api_key"] = ENV["NASA_API_KEY"]
-  #   end
-  #
-  #   raw_data = JSON.parse(response.body, symbolize_names: true)
-  #
-  #   dangeroids = raw_data[:near_earth_objects][:"#{dangerday}"].select do |asteroid|
-  #     asteroid[:is_potentially_hazardous_asteroid]
-  #   end
-  #
-  #   dangeroids.map do |asteroid|
-  #     Asteroid.new(asteroid)
-  #   end
-  # end
+  def self.conn
+    Faraday.new("https://api.nasa.gov/neo/rest/v1/feed")
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
